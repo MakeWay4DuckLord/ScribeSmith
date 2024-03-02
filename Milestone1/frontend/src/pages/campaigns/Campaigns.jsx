21ebc89dfb785a2a4f6c8faa2769a7b5d7e28e83
Sam Stone <sjstone3@ncsu.edu>
2024-03-02 10:46:40 -0500

    Created campaign page with dummy data from api endpoints

--- Milestone1/frontend/src/pages/campaigns/Campaigns.jsx
+++ Milestone1/frontend/src/pages/campaigns/Campaigns.jsx
@@ -1,11 +1,40 @@
 import { Link } from "react-router-dom";
 import CampaignCard from "../../components/CampaignCard/CampaignCard";
-import "./campaign.css"
+import "./campaigns.css";
+import api from "../../client/APIClient";
+import { useEffect, useState } from "react";
 
 export default function Campaigns() {
+    const [userCampaigns, setUserCampaigns] = useState([]);
+    const [ownerNames, setOwnerNames] = useState({});
+
+    //TODO: Change  getUserCampaigns() for the param to be the current user logged in
+    useEffect(() => {
+        api.getUserCampaigns(1).then(campaigns => {
+            setUserCampaigns(campaigns);
+
+            if (campaigns.length !== 0) {
+                Promise.all(
+                    campaigns.map(campaign =>
+                      api.getUser(campaign.ownerId).then(user => ({
+                        ownerId: campaign.ownerId,
+                        ownerName: user.name
+                      }))
+                    )
+                  ).then(ownerNamesArr => {
+                    let newOwnerNames = {};
+                    ownerNamesArr.forEach(owner => {
+                        newOwnerNames[owner.ownerId] = owner.ownerName;
+                    });
+                    setOwnerNames(newOwnerNames);
+                });
+            }
+        });
+    }, []);
+
     return (
         <>
-            <div className="campaign-page">
+            <div className="campaigns-page">
                 <div className="content">
                     <div className="buttons">
                         <Link to="/join-campaign"><button>Join Campaign</button></Link>
@@ -15,10 +44,11 @@ export default function Campaigns() {
                     <h1>MY CAMPAIGNS</h1>
 
                     <div className="campaign-container">
-                        <CampaignCard name="Campaign Name" owner="User 1" description="description" />
-                        <CampaignCard name="Campaign Name" owner="User 1" description="description" />
-                        <CampaignCard name="Campaign Name" owner="User 1" description="description" />
-                        <CampaignCard name="Campaign Name" owner="User 1" description="description" />
+                        {userCampaigns.map(campaign => (
+                            <Link to={`/campaign/${campaign.id}`} key={campaign.id}>
+                                <CampaignCard name={campaign.name} owner={ownerNames[campaign.ownerId]} description={campaign.description} />
+                            </Link>
+                        ))}
                     </div>
                     
                 </div>                
