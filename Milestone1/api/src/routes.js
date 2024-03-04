1ca890bfccc560b46397d844725bb76ac547518d
llkoger <llkoger@ncsu.edu>
2024-03-04 16:17:13 -0500

    mocked up patch requests and fixed some misc stuff

--- Milestone1/api/src/routes.js
+++ Milestone1/api/src/routes.js
@@ -93,6 +93,7 @@ router.post('/campaigns', (req, res) => {
     while (Object.values(campaigns).find(campaign => campaign.joinCode == newCampaign.joinCode) != undefined) {
         newCampaign.joinCode = makeJoinCode(5);
     }
+    // TODO: make sure all fields are validated!
     newCampaign.ownerId = req.body.ownerId;
     newCampaign.name = req.body.name;
     newCampaign.description = req.body.description;
@@ -138,28 +139,61 @@ router.delete('/campaigns/:campaignId/users/:userId', (req, res) => {
 });
 
 //update campaign description
-router.patch('campaigns/:campaignId/description', (req, res) => {
-
+router.patch('/campaigns/:campaignId/description', (req, res) => {
+    const campaignId = parseInt(req.params.campaignId);
+    const campaign = campaigns[campaignId];
+    if (!campaign) {
+        res.status(401).json({ "error": "Campaign not found" });
+        return;
+    }
+    if (req.body.description == undefined || req.body.description == "") {
+        res.status(400).json({ "error": "Invalid description" });
+        return;
+    }
+    campaign.description = req.body.description;
+    res.status(200).json({"message": "success"});
 });
 
 //update campaign tags
-router.patch('campaigns/:campaignId/tags', (req, res) => {
-
+router.patch('/campaigns/:campaignId/tags', (req, res) => {
+    const campaignId = parseInt(req.params.campaignId);
+    const campaign = campaigns[campaignId];
+    if (!campaign) {
+        res.status(401).json({ "error": "Campaign not found" });
+        return;
+    }
+    if (req.body.tags == undefined || !Array.isArray(req.body.tags)) {
+        res.status(400).json({ "error": "Invalid tags list" });
+        return;
+    }
+    campaign.tags = req.body.tags;
+    res.status(200).json({"message": "success"});
 });
 
 //update banner image
-router.patch('campaigns/:campaignId/banner', (req, res) => {
-
+router.patch('/campaigns/:campaignId/banner', (req, res) => {
+    const campaignId = parseInt(req.params.campaignId);
+    const campaign = campaigns[campaignId];
+    if (!campaign) {
+        res.status(401).json({ "error": "Campaign not found" });
+        return;
+    }
+    if (req.body.banner == undefined || req.body.banner == "") {
+        res.status(400).json({ "error": "Invalid banner image link" });
+        return;
+    }
+    campaign.banner = req.body.banner;
+    res.status(200).json({"message": "success"});
 });
 
 //get notes by creator and campaign (could this be removed?)
-router.get('campaigns/:campaignId/notes/users/:userId', (req, res) => {
+router.get('/campaigns/:campaignId/notes/users/:userId', (req, res) => {
     // note - this request will always need to filter out non-viewable notes
     // based on authentication
 });
 
 //get notes by creator and campaign and tags
-router.get('campaigns/:campaignId/notes/users/:userId/tags/:tagString', (req, res) => {
+router.get('/campaigns/:campaignId/notes/users/:userId/tags/:tagString', (req, res) => {
     // TODO: this url seems a little fucked up but if i don't put
     // tags as a url parameter then it has to go in the body
     // which is supposed to be empty
