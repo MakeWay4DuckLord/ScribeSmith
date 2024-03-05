6b7d2cbaa6294138658914dcea7192f8710c1a26
Declan Smith <dwsmith7@ncsu.edu>
2024-03-05 14:48:39 -0500

    Refactored MyNotes to utilize a NoteBrowser component
    pair programming: Lucien, Declan

--- Milestone1/frontend/src/pages/my-notes/MyNotes.jsx
+++ Milestone1/frontend/src/pages/my-notes/MyNotes.jsx
@@ -1,9 +1,6 @@
-import './myNotes.css'
-import { FaCirclePlus } from "react-icons/fa6";
-import { IoIosSearch } from "react-icons/io";
-import NotePreview from "../../components/NotePreview/NotePreview";
-import Note from "../../components/Note/Note";
-import Tag from "../../components/Tag/Tag";
+// import './myNotes.css'
+
+import NoteBrowser from "../../components/NoteBrowser/NoteBrowser";
 import api from "../../client/APIClient";
 
 import { useEffect, useState } from 'react';
@@ -29,35 +26,8 @@ export default function MyNotes() {
 
 
     return(
-        <div className='myNotes'>
-            <aside>
-                <h1>My Notes</h1>
-                <search>
-                    <input type="text" placeholder='Search...' />
-                    <IoIosSearch className='searchIcon' />
-
-                </search>
-
-                <div className='tagList'>
-                    <Tag content={"session1"}/>
-                    <Tag content={"session2"}/>
-                    <Tag content={"session4"}/>
-                    <FaCirclePlus className='addTagIcon' />
-                </div>
-
-                <div className='note-container'>
-                    {myNotes.map(note => (
-                        <Note id={note.id} title={note.title} content={note.content} tags={note.tags}/>
-                    ))}
-
-                    {/* hard coded notes for testing and such */}
-                    {/* <Note id="1" title="Note Title" content="In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy." tags={["session1", "tag2"]}/>
-                    <Note id="2" title="Note Title" content="In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy." tags={["session2", "tag3"]}/> */}
-
-                </div>
-                <main>
-                </main>
-            </aside>
-        </div>
+        // <div className='myNotes'>
+        <NoteBrowser title='My Notes' notes={myNotes}></NoteBrowser>
+        // </div>
     );
 }
