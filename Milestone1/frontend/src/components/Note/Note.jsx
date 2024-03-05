c2bf8b61b84bc455599f095d0891b8c606e77d46
Declan Smith <dwsmith7@ncsu.edu>
2024-03-05 13:47:45 -0500

    Added props to Note.jsx. Pair programmed with Lucien

--- Milestone1/frontend/src/components/Note/Note.jsx
+++ Milestone1/frontend/src/components/Note/Note.jsx
@@ -1,7 +1,30 @@
 import Tag from "../Tag/Tag";
+import { useEffect, useState } from "react";
 
-export default function Note(note) {
+
+export default function Note({id, title, content, tags}) {
+    const [noteTags, setTags] = useState([]);
+
+    useEffect(() => {
+        setTags(JSON.parse(tags));
+    })
+
     return (
-        <></>
+        <>
+        <div className="notePreviewComponent">
+            <header>
+                <img className="icon" src="https://robohash.org/veniamdoloresenim.png?size=64x64&set=set1" alt="user icon" />
+                <h2>{title}</h2>
+                <time>1hr ago</time>
+            </header>
+            
+            <p>{content}</p>
+            <div className="tagContainer">
+                {noteTags.map(tag => (
+                    <Tag content={tag}/>
+                ))}
+            </div>
+        </div>
+        </>
     );
 }
