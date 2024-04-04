82ec7cdf701ad1efa54b817d927d3320a4b90997
llkoger <llkoger@ncsu.edu>
2024-04-04 13:42:48 -0400

    added foreign keys

--- Milestone2/database/db_schema/scribesmith.sql
+++ Milestone2/database/db_schema/scribesmith.sql
@@ -9,16 +9,6 @@
 
 /* i don't know why those ^ are here... but i left them in */
 
-CREATE TABLE IF NOT EXISTS `campaign` (
-  `cpn_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `cpn_owner_id` int(10) unsigned NOT NULL,
-  `cpn_name` varchar(50) NOT NULL,
-  `cpn_banner` varchar(500),
-  `cpn_description` varchar(1200),
-  `cpn_join_code` char(5),
-  PRIMARY KEY (`cpn_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE IF NOT EXISTS `user` (
   `usr_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `usr_email` varchar(90) NOT NULL,
@@ -30,12 +20,26 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`usr_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE IF NOT EXISTS `campaign` (
+  `cpn_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cpn_owner_id` int(10) unsigned NOT NULL,
+  `cpn_name` varchar(50) NOT NULL,
+  `cpn_banner` varchar(500),
+  `cpn_description` varchar(1200),
+  `cpn_join_code` char(5),
+  PRIMARY KEY (`cpn_id`),
+  CONSTRAINT `FK_CPN_USR` FOREIGN KEY (`cpn_owner_id`) REFERENCES user(`usr_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE IF NOT EXISTS `note` (
   `note_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `note_owner_id` int(10) unsigned NOT NULL,
+  `note_campaign_id` int(10) unsigned NOT NULL,
   `note_title` varchar(60) NOT NULL,
   `note_text` varchar(Max),
-  PRIMARY KEY (`note_id`)
+  PRIMARY KEY (`note_id`),
+  CONSTRAINT `FK_NOTE_USR` FOREIGN KEY (`note_owner_id`) REFERENCES user(`usr_id`),
+  CONSTRAINT `FK_NOTE_CPN` FOREIGN KEY (`note_campaign_id`) REFERENCES campaign(`cpn_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `tag` (
