200a3d47aaaa17cc857312cac4d3c01ebceccc41
Declan Smith <dwsmith7@ncsu.edu>
2024-04-04 13:43:08 -0400

    Installed MUI, added a basic theme to main.jsx, and fixed a bug in Campaign and MyNotes

--- Milestone2/frontend/src/pages/campaign/Campaign.jsx
+++ Milestone2/frontend/src/pages/campaign/Campaign.jsx
@@ -43,7 +43,7 @@ export default function Campaign() {
             navigate("/login");
         })
         
-    }, [id]);
+    }, [campaignId]);
 
     if(error !== "") {
         return <h2>{error}</h2>
